4adf3eabeffe9d5e31a272f89d5515bdee834822
zhora <vikulov33@rambler.ru>
2018-02-14 15:55:35 +0300

    a shadow is cast on the canvas, the histogram is changed, and its color is rounded off, counting statistics

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,19 @@
 window.renderStatistics=function(ctx,names,times){
-  ctx.fillStyle='rgba(256,256,256,1.0)';
-  ctx.strokeRect(100,10,420,270);
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110,20,420,270);
+  ctx.fillStyle = '#ffffff';
   ctx.fillRect(100,10,420,270);
   console.log(times);
   ctx.fillStyle= '#000';
-  ctx.fillText('Ура вы победили !',120,40);
+  ctx.textBaseline = 'hanging';
+
+  ctx.fillText('Ура вы победили !',130,30);
+  ctx.fillText('Список результатов: !',130,50);
   ctx.font='16px PT Mono';
+
+
+
 
 //сделать функцию пуе
   // var max=-1;
@@ -50,29 +58,56 @@ window.renderStatistics=function(ctx,names,times){
   var sss=findMaxTime(times);
   console.log("Смотри ниже ");
   console.log(sss);
-  console.log(maxIndex);
 
+  // var histogramWidth=150;
+
+
+ 
+  // ctx.fillText('Худшее время: '+ sss.toFixed(2)+' мс У игрока '+names[i],120,60);
   var histogramWidth=150;
 
-  var step=histogramWidth/(sss-0);
-  ctx.fillText('Худшее время: '+ sss.toFixed(2)+' мс У игрока '+names[maxIndex],120,60);
+  var columnX=130;
+  //initialX начальные координаты X которые я рисую
+  var initialX=130;
+
+  var columnWidth=40;
+  //indent отступ между полосками 
+  var indent=90;
+
 
   //barHeigth высота одной полоски
   var barHeigth=20;
-  //indent отступ между полосками 
-  var indent=40;
-  //initialX начальные координаты X которые я рисую
-  var initialX=120;
-  //initialY начальные координаты У которые я рисую
-  var initialY=80;
+
+
+  // //initialY начальные координаты У которые я рисую
+  // var initialY=80;
   //lineHeight высота столбика
   var lineHeight=15;
 
 
+  
+
+
+
+  var step=histogramWidth/(sss);
+
   for(var i=0;i<times.length;i++){
+    var columnHeight=step*times[i];
+    var columnY=ctx.canvas.clientHeight-columnHeight-60;
+    ctx.fillStyle='#000';
+    ctx.fillText(times[i].toFixed(0),columnX+indent*i,columnY-15);
 
 
-  ctx.fillRect(initialX,initialY+indent*i,times[i]*step,barHeigth);
-  ctx.fillText(names[i],initialX + histogramWidth,initialY+lineHeight+indent*i);
+    if (names[i]==='Вы'){
+      ctx.fillStyle='rgba(255,0,0,1)';
+    } else{
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+  
+    }
+
+    ctx.fillRect(initialX + indent * i, columnY, columnWidth, columnHeight);
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], initialX + indent * i, columnY + columnHeight + 5);
+
     }
   };
